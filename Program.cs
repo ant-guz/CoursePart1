3bfb5e13547fe89b7dab46de1b21faa6536f9887
Anton Guz <driven.ua@gmail.com>
2023-05-25 22:47:12 +0300

    Rename solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Starting C# course - Lesson1 ------");
+            Console.WriteLine("Enter your name:");
+            string enteredName = Console.ReadLine();
+            Console.WriteLine($"Hello, {enteredName}");
+        }
+    }
+}
