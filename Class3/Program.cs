2147222310c88ebbe2736dc816093d0e9f16d248
ant-guz <anton.guz87@gmail.com>
2023-05-28 15:53:33 +0300

    Add lesson 3

---
+++ Class3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            System.Console.WriteLine("----- Starting C# course - Lesson3 ------");
+
+        }
+    }
+}
